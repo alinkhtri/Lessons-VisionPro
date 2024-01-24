e12e1c9252844ec0c875408c7556b43fdd0c5bb3
Alin Khatri <alin.khatri@tekkon.com.np>
2024-01-24 22:38:20 +0545

    Initial Commit

---
+++ LessonsVideoPlayer/LessonsVideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LessonsVideoPlayerApp.swift
+//  LessonsVideoPlayer
+//
+//  Created by Alin Khatri on 24/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct LessonsVideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
